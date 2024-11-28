3cbf434e742a6ac3de1d2044f1214a86e7dc424e
shahob-atm <shahobiddinatamurodov@gmail.com>
2024-11-28 16:53:05 +0500

    fixed

--- src/main/java/org/example/backend/config/WebMvcConfig.java
+++ src/main/java/org/example/backend/config/WebMvcConfig.java
@@ -24,37 +24,27 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/static/"); // Statik fayllar joylashuvi
     }
 
-    @Override
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/{spring:[a-zA-Z0-9-_]+}")
-                .setViewName("forward:/index.html");
-        registry.addViewController("/**/{spring:[a-zA-Z0-9-_]+}")
-                .setViewName("forward:/index.html");
-        registry.addViewController("/{spring:[a-zA-Z0-9-_]+}/**")
-                .setViewName("forward:/index.html");
+    private void serveDirectory(ResourceHandlerRegistry registry, String endpoint, String location) {
+        // 1
+        String[] endpointPatterns = endpoint.endsWith("/")
+                ? new String[]{endpoint.substring(0, endpoint.length() - 1), endpoint, endpoint + ""}
+                : new String[]{endpoint, endpoint + "/", endpoint + "/"};
+        registry
+                // 2
+                .addResourceHandler(endpointPatterns)
+                .addResourceLocations(location.endsWith("/") ? location : location + "/")
+                .resourceChain(false)
+                // 3
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    public Resource resolveResource(HttpServletRequest request, String requestPath,
+                                                    List<? extends Resource> locations, ResourceResolverChain chain) {
+                        Resource resource = super.resolveResource(request, requestPath, locations, chain);
+                        if (nonNull(resource)) {
+                            return resource;
+                        }
+                        return super.resolveResource(request, "/index.html", locations, chain);
+                    }
+                });
     }
-
-//    private void serveDirectory(ResourceHandlerRegistry registry, String endpoint, String location) {
-//        // 1
-//        String[] endpointPatterns = endpoint.endsWith("/")
-//                ? new String[]{endpoint.substring(0, endpoint.length() - 1), endpoint, endpoint + ""}
-//                : new String[]{endpoint, endpoint + "/", endpoint + "/"};
-//        registry
-//                // 2
-//                .addResourceHandler(endpointPatterns)
-//                .addResourceLocations(location.endsWith("/") ? location : location + "/")
-//                .resourceChain(false)
-//                // 3
-//                .addResolver(new PathResourceResolver() {
-//                    @Override
-//                    public Resource resolveResource(HttpServletRequest request, String requestPath,
-//                                                    List<? extends Resource> locations, ResourceResolverChain chain) {
-//                        Resource resource = super.resolveResource(request, requestPath, locations, chain);
-//                        if (nonNull(resource)) {
-//                            return resource;
-//                        }
-//                        return super.resolveResource(request, "/index.html", locations, chain);
-//                    }
-//                });
-//    }
 }
